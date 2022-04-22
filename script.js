912593f79599147c7e3f2a2d7a0e62c152b3cc38
Giulia-92 <CIONCO.GIULIA@GMAIL.COM>
2022-04-22 20:23:37 +0200

    vue-todolist

---
+++ script.js
@@ -0,0 +1,52 @@
+const toodolist = new Vue ({
+    el: '#lista',
+    data:{
+        tiramisu: [
+            {
+                check:false,
+                nomeIngrediente:'savoiardi',
+            },
+             {
+                check:false,
+                nomeIngrediente:'zucchero',
+            },
+            {
+                check:false,
+                nomeIngrediente:'mascarpone',
+            },
+            {
+                check:false,
+                nomeIngrediente:'tuorli',
+            },
+            {
+                check:false,
+                nomeIngrediente:'caffe',
+            },
+            {
+                check:false,
+                nomeIngrediente:'cacao',
+            },
+            {
+                check:false,
+                nomeIngrediente:'marsala',
+            }
+        ],
+        nuovoIngrediente:'',
+    },
+    methods: {
+        aggiungi(i){
+        const ingrediente = {
+            check: false,
+            nomeIngrediente: this.nuovoIngrediente,
+        }
+           this.tiramisu.push(ingrediente)
+           this.nuovoIngrediente = '';
+
+        },
+        rimuovi(i){
+            this.tiramisu.splice(i,1)
+        }
+       
+    },
+
+})
